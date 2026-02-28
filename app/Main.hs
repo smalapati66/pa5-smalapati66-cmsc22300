3e81a8139e05ae2e864dffab2f7090cf753af297
Siddarth Malapati <155690174+smalapati66@users.noreply.github.com>
2026-02-28 17:58:14 -0600

    finished pt1 of pa5

--- app/Main.hs
+++ app/Main.hs
@@ -2,3 +2,28 @@ module Main where
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
+
+newtype Probability a
+  = Probability { getProbabilities :: [(a, Double)]}
+    deriving Show
+
+instance Functor Probability where
+  fmap f (Probability as) = Probability (map (\(a, p) -> (f a, p)) as)
+
+instance Applicative Probability where
+  pure a = Probability [(a, 1.0)]
+  Probability fs <*> Probability as = 
+    Probability([(f a, p*q) | (f,p) <- fs, (a,q) <- as])
+
+instance Monad Probability where
+  Probability as >>= f =
+    Probability $
+      concatMap
+        (\(a, p) ->
+          let Probability bs = f a
+          in map (\(b, q) -> (b, p * q)) bs
+        )
+        as
+
+roll :: Probability Int
+roll = Probability [ (i,1/6) | i <- [1..6] ]
